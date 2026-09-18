33eddbca4b2eba81ec05149231ef5d497b46999f
ellisf09 <ellisfox28@hotmail.com>
2026-09-18 11:50:44 +0100

    fixed logic

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def main():
         log_state()
         for event in pygame.event.get():
             for event in pygame.event.get():
-                if event.type = pygame.QUIT:
+                if event.type == pygame.QUIT:
                     return
         screen.fill("black")
         pygame.display.flip()
